8beaf1c3ff812ad698e209dc85e7e360abe2c6c2
akashdurai4274 <akashdurai4274@gmail.com>
2026-02-01 12:27:33 +0530

    Update CORS middleware to disable credentials support

--- backend/app/main.py
+++ backend/app/main.py
@@ -57,7 +57,7 @@ def create_app() -> FastAPI:
         CORSMiddleware,
         # allow_origins=settings.CORS_ORIGINS,
         allow_origins= ["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
